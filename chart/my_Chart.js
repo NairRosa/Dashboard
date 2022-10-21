76bed9389b0e840617cf2e31fec8a8f4a8f13ae5
NairNeta <contatonairrosa@gmail.com>
2022-10-21 17:45:17 -0300

    Add files via upload

---
+++ chart/my_Chart.js
@@ -0,0 +1,72 @@
+const ctx = document.getElementById('myChart').getContext('2d');
+const earning = document.getElementById('earning').getContext('2d');
+
+const myChart = new Chart(ctx, {
+    type: 'polarArea',
+    data: {
+        labels: ['Dress', 'Bag', 'T-Shirt', 'Hat', 'Body', 'Swetter'],
+        datasets: [{
+            label: 'Vendas',
+            data: [15, 12, 6, 18, 7, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+const ear = new Chart(earning, {
+    type: 'bar',
+    data: {
+        labels: ['Dress', 'Bag', 'T-Shirt', 'Hat', 'Body', 'Swetter'],
+        datasets: [{
+            label: 'Valor',
+            data: [1120, 1990, 480, 900, 890, 3670],
+            backgroundColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
